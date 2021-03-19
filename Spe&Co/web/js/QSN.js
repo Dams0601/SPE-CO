c4f1025d88fa7b21e2bc40c3629f34b9f4f2fe8d
TPE <60327832+Dams0601@users.noreply.github.com>
2021-03-19 22:30:40 +0100

    Add files via upload

---
+++ Spe&Co/web/js/QSN.js
@@ -0,0 +1,32 @@
+var timer = 4000;
+
+var i = 0;
+var max = $('.card').length;
+ 
+	$(".card").eq(i).addClass('active').css('left','10%');
+	$(".card").eq(i + 1).addClass('active').css('left','40%');
+	$(".card").eq(i + 2).addClass('active').css('left','70%');
+ 
+
+	setInterval(function(){ 
+
+		$(".card").removeClass('active');
+
+		$(".card").eq(i).css('transition-delay','0.25s');
+		$(".card").eq(i + 1).css('transition-delay','0.5s');
+		$(".card").eq(i + 2).css('transition-delay','0.75s');
+
+		if (i < max-3) {
+			i = i+3; 
+		}
+
+		else { 
+			i = 0; 
+		}  
+
+		$(".card").eq(i).css('left','10%').addClass('active').css('transition-delay','1.25s');
+		$(".card").eq(i + 1).css('left','40%').addClass('active').css('transition-delay','1.5s');
+		$(".card").eq(i + 2).css('left','70%').addClass('active').css('transition-delay','1.75s');
+	
+	}, timer);
+ 
