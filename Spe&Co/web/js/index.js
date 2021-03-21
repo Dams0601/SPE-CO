18451fe69307b5bc5a3d91206c78d3badc73c432
TPE <60327832+Dams0601@users.noreply.github.com>
2021-03-21 11:49:25 +0100

    Add files via upload

--- Spe&Co/web/js/index.js
+++ Spe&Co/web/js/index.js
@@ -1,3 +1,15 @@
+/* ENTREE DU SITE */
+setTimeout(function(){
+    document.querySelector('.entre').classList.remove('active');
+    document.querySelector('.entre').classList.add('disabled');
+    document.querySelector('.page').classList.remove('disabled');
+    document.querySelector('.page').classList.add('active');
+}, 2000)
+
+
+
+
+/* HAMBURGER */
 const hamburger = document.querySelector('#hamburger');
 const navContent = document.querySelector('.navbar-content');
 
