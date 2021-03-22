617f13c8d022c08c11e079ea949f69aac8a419fe
Damien <60327832+Dams0601@users.noreply.github.com>
2021-03-22 21:59:48 +0100

    Add files via upload

--- Spe&Co/web/js/inscription.js
+++ Spe&Co/web/js/inscription.js
@@ -1,13 +1,8 @@
-function inscription(){
-	console.log('BONJOUR')
-	location.href = "../html/connexion.html";
-}
-
 /* Signup form string lenght check */
 document.querySelector('#submit').addEventListener('click', function(e){
     if(document.getElementById("password").value.length > 3 && document.getElementById("mail").value.length > 3 && document.querySelector('#confirm_password').value==document.querySelector('#password').value){
 		document.getElementById("form").submit();
-        inscription();
+        location.href = "../html/connexion.html";
 	}
 	else{
 		alert("Les coordonées entrées sont erronées.");
