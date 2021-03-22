643806732e949a6d95289ffd5f10c5636b9b8f58
Damien <60327832+Dams0601@users.noreply.github.com>
2021-03-22 21:55:27 +0100

    Add files via upload

--- Spe&Co/web/js/connexion.js
+++ Spe&Co/web/js/connexion.js
@@ -1,13 +1,9 @@
-document.querySelector('.connexion').addEventListener('click',function(e){
-	location.href = "../html/index.html";
-})
-
 /* Signup form string lenght check */
-function submit_form(){
-	if(document.getElementById("password").value.length > 3 && document.getElementById("mail").value.length > 3){
+document.querySelector('#submit').addEventListener('click', function(e){
+    if(document.getElementById("password").value.length > 3 && document.getElementById("mail").value.length > 3){
 		document.getElementById("form").submit();
 	}
 	else{
-		alert("Vos identifiants ne peuvent pas être aussi court !");
+		alert("Les coordonées entrées sont erronées.");
 	}
-}
+})
