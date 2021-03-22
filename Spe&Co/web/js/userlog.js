d06386ef6b3ba85efa97b4945a65dd21869e4bec
Alexstone24 <80351314+Alexstone24@users.noreply.github.com>
2021-03-22 17:31:39 +0100

    Add files via upload
    le js

---
+++ Spe&Co/web/js/userlog.js
@@ -0,0 +1,16 @@
+/* Signin button redirection */
+function goToSignIn(){
+    window.location.href = "../html/signin.html";
+}
+
+/* Signup form string lenght check*/
+function submit_form(){
+	if(document.getElementById("password").value.length > 2 && document.getElementById("mail").value.length > 2){
+		document.getElementById("form").submit();
+		console.log(document.getElementById("password").value.length);
+		console.log(document.getElementById("mail").value.length);
+	}
+	else{
+		alert("Vos identifiants ne peuvent pas être aussi court !");
+	}
+}
